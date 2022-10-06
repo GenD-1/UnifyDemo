df0c51627ef22ad4768df8fd147281114ee2f25c
blugee <blugee>
2022-10-06 08:54:42 -0400

    changes for css

--- src/components/Testing/MicroPhone.jsx
+++ src/components/Testing/MicroPhone.jsx
@@ -3,7 +3,7 @@ import { Mic, MicOff } from 'react-feather'
 const AudioButton = ({ active, onClick }) => {
   return (
     <button
-      className={`p-3 rounded-full ${active ? 'bg-brand-100' : 'bg-red-600'}`}
+      className={`p-3 rounded-full absolute top-[45%] left-[45%] z-[999]  ${active ? 'bg-brand-100' : 'bg-red-600'}`}
       onClick={onClick}
     >
       {active ? <Mic size={25} /> : <MicOff size={25} />}
